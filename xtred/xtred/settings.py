e0026c724b677467137cad18b6a2bfe6f6543489
el <apply.powder.0p@icloud.com>
2024-09-09 14:08:35 +0300

    add media

--- xtred/xtred/settings.py
+++ xtred/xtred/settings.py
@@ -156,7 +156,8 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'collected_static'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/ovaynise/engineer/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
