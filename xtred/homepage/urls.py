efcfdfb578fbe8d2ce6118e027d106032fb59c62
el <apply.powder.0p@icloud.com>
2024-09-16 22:15:19 +0300

    test

--- xtred/homepage/urls.py
+++ xtred/homepage/urls.py
@@ -1,4 +1,3 @@
-from auth_telegram import telegram_auth
 from django.urls import path
 
 from . import views
@@ -6,5 +5,4 @@ from . import views
 app_name = "homepage"
 urlpatterns = [
     path("", views.index, name="index"),
-    path('telegram_auth/', telegram_auth, name='telegram_auth'),
 ]
